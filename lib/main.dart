16c0ceb59435dbdbf0ce9fb276abe50aba1cde8b
NaponTunglukmongkol <60070037@kmitl.ac.th>
2019-02-11 11:38:01 +0700

    Initial project

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import './ui/login.dart';
+import './ui/register.dart';
+import './ui/mainpage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{ //Change screen
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: MyHomePage(),
+      initialRoute: '/',
+      routes: {
+        "/": (context) => LoginPage(),
+        "/register": (context) => RegisterPage(),
+        "/main": (context) => MPage(),
+      },
+    );
+  }
+
+}
---
+++ lib/ui/register.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return RegisterPageState();
+  }
+}
+
+class RegisterPageState extends State<RegisterPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
